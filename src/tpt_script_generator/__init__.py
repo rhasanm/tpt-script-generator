34e995fec7abea02d5aaebc494457d8876201efc
Md. Rakibul Hasan <hasanrakibul.masum@gmail.com>
2024-08-08 12:53:38 +0600

    re export operators

--- src/tpt_script_generator/__init__.py
+++ src/tpt_script_generator/__init__.py
@@ -3,5 +3,6 @@
 # SPDX-License-Identifier: MIT
 
 from tpt_script_generator.ddl_operator import DDLOperator
+from tpt_script_generator.update_operator import UpdateOperator
 from tpt_script_generator.script import TPTScript
 from tpt_script_generator.step import Step
