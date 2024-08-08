0aa781cfb688fa0b832eb0e8283703fd429ca3ac
Md. Rakibul Hasan <hasanrakibul.masum@gmail.com>
2024-08-08 12:15:21 +0600

    attribute config updated

--- src/tpt_script_generator/base_operator.py
+++ src/tpt_script_generator/base_operator.py
@@ -24,10 +24,7 @@ class Operator(ABC):
         if self.attributes:
             definition += "    ATTRIBUTES\n    (\n"
             for attr_key, (attr_type, attr_value) in self.attributes.items():
-                if attr_type != AttributeType.VARCHAR.value:
-                    definition += f"        {attr_type} {attr_key} = {attr_value},\n"
-                else:
-                    definition += f"        {attr_type} {attr_key} = '{attr_value}',\n"
+                definition += f"        {attr_type} {attr_key} = {attr_value},\n"
             definition = definition.rstrip(",\n")  # Remove the trailing comma
             definition += "\n    );\n"
         return definition
